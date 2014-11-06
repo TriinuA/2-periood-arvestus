eb956b71d49dc623b1576b5cc93f75db2bd5fca8
TriinuA <triinu.a@gmail.com>
2014-11-06 20:56:39 +0200

    Complited

--- ylesanne-4/index.php
+++ ylesanne-4/index.php
@@ -1,3 +1,7 @@
+<?php 
+	setcookie("arvestus",  "Cookie on olemas", time() +20);
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,7 +10,19 @@
   </head>
 
   <body>
-    <!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
-    <pre><?php print_r($_COOKIE); ?></pre>
+    
+
+    <?php
+    	 
+		/*isset($_COOKIE["arvestus"]) ? $_COOKIE["arvestus"] : "Cookie-t ei ole!";*/  //ei tulnud välja
+
+    	if (isset($_COOKIE["arvestus"])) {
+    		echo $_COOKIE["arvestus"];
+    	}
+    		else
+    			echo "Cookie-t ei ole!";
+
+     ?>
+
   </body>
 </html>
