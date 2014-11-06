a1a8e19de45e09d4af1f1fbe8777b4e9d4ed4abc
TriinuA <triinu.a@gmail.com>
2014-11-06 10:32:41 +0200

    not finished

--- ylesanne-3/result.php
+++ ylesanne-3/result.php
@@ -9,6 +9,40 @@
     <!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
     <pre><?php print_r($_POST); ?></pre>
 
+   <?php 
+     	if (isset($_POST["submit"])){
+     		if (isset($_POST ["username"]) && isset(isset($_POST ["sex"])){
+		    		$username = $_POST["username"];
+		    		$sex = $_POST["sex"];
+		    		echo "Tere "  $username "! Tundub, et olete " $sex ".";
+		    	}
+		    	else {
+		    		if (isset($_POST ["username"]){
+		    			$username = $_POST["username"];
+		    			echo "Tere "  $username "! Palun minge tagasi ja valige sugu.";				
+		    		}
+		    		elseif (isset($_POST ["sex"])){
+		    				$sex = $_POST["sex"];
+		    				echo "Tundub, et olete " $sex ", kuid palun minge tagasi ja sisestage nimi.";
+		    			}
+		    			else
+		    				echo "Palun minge tagasi ja täitke väljad.";
+		    	}
+     	}
+     		else
+	    		echo "Tundub, et sattusid siia otseteed pidi.";
+
+      ?>
+     ?>
+
+    
+
+ 		
+
+      ?>
+      
+
+
     <div><a href="index.php">Mine tagasi</a></div>
   </body>
 </html>
